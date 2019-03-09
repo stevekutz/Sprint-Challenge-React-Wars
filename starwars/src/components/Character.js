3799d13394d0726becce0ffb420d1f84b6b18af9
stevekutz <stkutz@gmail.com>
2019-03-09 14:09:31 -0600

    MVP completed, verifying array layout vs. doing stretch

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,7 +8,50 @@ const Character = (props) => {
   return (
 
     <div className = "main-container">
-      <h2 className = 'name'> Name: {props.character.name}</h2>
+      <h1 className = 'name'> Name: {props.character.name}</h1>
+
+      <div className = "character-container">
+        <div className = 'physical'>
+          <h2> Physical Attrbutes </h2>
+
+          <h3 className = 'physical_attr'> Gender: {props.character.gender}</h3>
+          <h3 className = 'physical_attr'> Hair: {props.character.hair_color}</h3>
+          <h3 className = 'physical_attr'> Height: {props.character.height} inches</h3>
+          <h3 className = 'physical_attr'> Homeworld: {props.character.homeworld}</h3>
+          <h3 className = 'physical_attr'> Mass: {props.character.mass} pounds</h3>
+          <h3 className = 'physical_attr'> Species: {props.character.species[0]} </h3>
+          <h3 className = 'physical_attr'> URL: {props.character.url} </h3>
+
+        </div>
+
+
+        <div className = "array_items">
+
+          <div className = "films ">
+            <h2> Films </h2>
+            <h3 className = 'film_attr'> {props.character.films[0]}</h3>
+            <h3 className = 'film_attr'>{props.character.films[0]}</h3>
+          </div>
+
+          <div className = "starships ">
+            <h2> Starships </h2>
+            <h3 className = 'starships_attr'> {props.character.vehicles[0]}</h3>
+            <h3 className = 'starships_attr'> {props.character.vehicles[0]}</h3>
+          </div>
+
+          <div className = "vehicles ">
+            <h2> Vehicles </h2>
+            <h3 className = 'vehicle_attr'> {props.character.vehicles[0]}</h3>
+            <h3 className = 'vehicle_attr'> {props.character.vehicles[0]}</h3>
+          </div>
+
+
+
+
+        </div>
+
+
+      </div>
 
 
 
@@ -16,7 +59,10 @@ const Character = (props) => {
 
 
 
-    </div>
+
+
+
+    </div> // end main-container
 
 
 
